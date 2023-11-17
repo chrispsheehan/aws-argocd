ac2d640c47c4fd897e0c5ef498f69b07b739800b
chrispsheehan <chrispsheehan@gmail.com>
2023-11-17 21:02:37 +0000

    fix

--- tf/bin/startup.sh
+++ tf/bin/startup.sh
@@ -24,14 +24,14 @@ sudo tar zxvf crictl-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-linux-amd64.tar.gz
 sudo su -s /bin/bash -c 'minikube start' ec2-user
 
-# install argocd
-echo installing argocd cli
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-rm argocd-linux-amd64
-
 # create argocd server
 echo creating argocd server
 sudo su -s /bin/bash -c "kubectl create namespace argocd" ec2-user
 sudo su -s /bin/bash -c "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml" ec2-user
 sudo su -s /bin/bash -c "kubectl patch svc argocd-server -n argocd -p '{\"spec\": {\"type\": \"LoadBalancer\"}}'" ec2-user
+
+# install argocd cli
+echo installing argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
