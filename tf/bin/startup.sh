350b7c8c313a29cdbddb3efebb3069682fb61cc9
chrispsheehan <chrispsheehan@gmail.com>
2023-11-17 20:18:53 +0000

    create argocd server

--- tf/bin/startup.sh
+++ tf/bin/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # install docker service
+echo installing docker
 sudo yum update -y
 sudo yum search docker
 sudo yum info docker
@@ -10,12 +11,8 @@ newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
-# install argocd
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-rm argocd-linux-amd64
-
 # install minikube
+echo installing minikube
 sudo yum install conntrack-tools.x86_64 -y
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -26,3 +23,15 @@ curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.28.0/c
 sudo tar zxvf crictl-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-linux-amd64.tar.gz
 sudo su -s /bin/bash -c 'minikube start' ec2-user
+
+# install argocd
+echo installing argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# create argocd server
+echo creating argocd server
+sudo su -s /bin/bash -c "kubectl create namespace argocd" ec2-user
+sudo su -s /bin/bash -c "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml" ec2-user
+sudo su -s /bin/bash -c "kubectl patch svc argocd-server -n argocd -p '{\"spec\": {\"type\": \"LoadBalancer\"}}'" ec2-user
