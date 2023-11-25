44c4b6c7b37f6d4e5ddab406ac3bb5d50abb351d
chrispsheehan <chrispsheehan@gmail.com>
2023-11-25 10:48:58 +0000

    test ns

--- tf/bin/orginal.sh
+++ tf/bin/orginal.sh
@@ -51,5 +51,5 @@ sudo su -s /bin/bash -c "argocd login --insecure localhost:8999 --username admin
 # create app
 echo creating app
 sudo su -s /bin/bash -c "kubectl create namespace test" ec2-user
-sudo su -s /bin/bash -c "argocd app create test --server localhost:8999 --dest-namespace argocd --dest-server https://kubernetes.default.svc --repo https://github.com/chrispsheehan/aws-argocd --path k8s --revision main --sync-policy automated" ec2-user
+sudo su -s /bin/bash -c "argocd app create test --server localhost:8999 --dest-namespace test --dest-server https://kubernetes.default.svc --repo https://github.com/chrispsheehan/aws-argocd --path k8s --revision main --sync-policy automated" ec2-user
 sudo su -s /bin/bash -c "argocd app sync test" ec2-user
