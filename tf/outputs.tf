c8d420b2af466e01da31d0a0cec6b59e38ec13a5
chrispsheehan <chrispsheehan@gmail.com>
2023-12-01 16:52:04 +0000

    output urls

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,7 @@
-output "ec2-dns" {
-  value = aws_instance.server.public_dns
+output "app-url" {
+  value = "http://${aws_instance.server.public_dns}:${local.argocd_app_port}"
+}
+
+output "argocd-url" {
+  value = "http://${aws_instance.server.public_dns}:${local.argocd_server_port}"
 }
